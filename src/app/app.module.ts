8d9cdd78796a33c9940934c1159b3aa480a92c23
Juan-David-Arroyave-Martinez <ju-an1994@hotmail.com>
2018-10-22 12:51:56 -0500

    add route protect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import {AngularFireAuthModule} from 'angularfire2/auth';
 
 import {environment} from '../environments/environment';
 import {AuthService } from './services/auth.service';
+import {AuthGuard} from './guards/auth.guard';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,7 @@ import {AuthService } from './services/auth.service';
     AngularFireAuthModule,
     AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
-  providers: [AuthService],
+  providers: [AuthService,AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
